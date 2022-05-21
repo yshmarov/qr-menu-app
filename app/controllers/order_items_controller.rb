0c176c0ca1bb1d691a521d96e0f44f62bbba6858
Yaro <yshmarov@gmail.com>
2022-05-21 18:01:54 +0200

    only draft order items can be deleted

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,10 +1,14 @@
 class OrderItemsController < ApplicationController
   def destroy
     @order_item = OrderItem.find(params[:id])
-    @order_item.destroy
-    @order_item.order.calculate_total_price
-    respond_to do |format|
-      format.html { redirect_to @order_item.order, notice: 'Item was removed from the order.' }
+    if @order_item.order.draft?
+      @order_item.destroy
+      @order_item.order.calculate_total_price
+      respond_to do |format|
+        format.html { redirect_to @order_item.order, notice: 'Item was removed from the order.' }
+      end
+    else
+      redirect_to @order_item.order, notice: 'Items can be removed only from draft orders.'
     end
   end
 end
