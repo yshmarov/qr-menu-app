fc60f0fe9b767c7c1673bf9832f77475d5a1d4a5
Yaro <yshmarov@gmail.com>
2022-05-21 12:51:04 +0200

    order items can be deleted - controller part

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,54 +1,9 @@
 class OrderItemsController < ApplicationController
-  before_action :set_order_item, only: %i[show edit update destroy]
-
-  def index
-    @order_items = OrderItem.all
-  end
-
-  def show; end
-
-  def new
-    @order_item = OrderItem.new
-  end
-
-  def edit; end
-
-  def create
-    @order_item = OrderItem.new(order_item_params)
-
-    respond_to do |format|
-      if @order_item.save
-        format.html { redirect_to order_item_url(@order_item), notice: 'Order item was successfully created.' }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @order_item.update(order_item_params)
-        format.html { redirect_to order_item_url(@order_item), notice: 'Order item was successfully updated.' }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def destroy
+    @order_item = OrderItem.find(params[:id])
     @order_item.destroy
     respond_to do |format|
-      format.html { redirect_to order_items_url, notice: 'Order item was successfully destroyed.' }
+      format.html { redirect_to @order_item.order, notice: 'Item was removed from the order.' }
     end
-  end
-
-  private
-
-  def set_order_item
-    @order_item = OrderItem.find(params[:id])
-  end
-
-  def order_item_params
-    params.require(:order_item).permit(:order_id, :menu_item_id, :quantity)
   end
 end
