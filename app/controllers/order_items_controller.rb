3191ec493e5c7ab981d8dbebf1dacb7428630454
Yaro <yshmarov@gmail.com>
2022-05-21 14:30:15 +0200

    fix price calculation

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,6 +2,7 @@ class OrderItemsController < ApplicationController
   def destroy
     @order_item = OrderItem.find(params[:id])
     @order_item.destroy
+    @order_item.order.calculate_total_price
     respond_to do |format|
       format.html { redirect_to @order_item.order, notice: 'Item was removed from the order.' }
     end
