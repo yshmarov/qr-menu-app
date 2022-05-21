e881be94dc4fe80d1f96d00583da2f7dc07a2ef7
Yaro <yshmarov@gmail.com>
2022-05-21 12:50:02 +0200

    order items can be deleted

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     collection do
       get :all
     end
-    resources :order_items
+    resources :order_items, only: :destroy
   end
   resources :menu_items do
     post :add_to_cart
