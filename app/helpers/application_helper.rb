c8dd6ef7b06767a754f676b5ae25270de845f87c
Yaro <yshmarov@gmail.com>
2022-05-22 12:12:33 +0200

    styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def active_link_to(path, &block)
     link_to path, class: ('active' if current_page?(path)), &block
   end
+
+  def order_pay_label(order)
+    'Complete order' if order.draft?
+  end
 end
