842d479be7e0b900857080f5210477eb525cf521
Yaro Shm <yshmarov@gmail.com>
2025-01-04 20:00:45 +0100

    more i18n

---
+++ test/controllers/orders_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class OrdersControllerTest < ActionDispatch::IntegrationTest
+  test 'index' do
+    get orders_url
+    assert_response :success
+  end
+end
---
+++ test/controllers/queue_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class QueueControllerTest < ActionDispatch::IntegrationTest
+  test 'index' do
+    get queue_url
+    assert_response :success
+  end
+end
