218aec403de77c7cb156be02b61d6296ff8239e1
Yaro Shm <yshmarov@gmail.com>
2025-01-04 22:40:11 +0100

    config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root to: redirect('/menu')
+  get "up" => "rails/health#show", as: :rails_health_check
+  root to: redirect("/menu")
 
-  get 'qr', to: 'menu#qr'
-  get 'menu', to: 'menu#index'
-  get 'queue', to: 'queue#index'
-  post 'add_to_cart/:menu_item_id', to: 'menu#add_to_cart', as: :add_to_cart
+  get "qr", to: "menu#qr"
+  get "menu", to: "menu#index"
+  get "queue", to: "queue#index"
+  post "add_to_cart/:menu_item_id", to: "menu#add_to_cart", as: :add_to_cart
 
   resources :orders, except: %i[destroy new create edit] do
     resources :order_items, only: :destroy
