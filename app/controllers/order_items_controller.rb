d1c5f5e63b53402fa97fb5cd56029d0435cc8387
Yaro Shm <yshmarov@gmail.com>
2025-01-04 20:23:51 +0100

    i18n order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,12 +1,10 @@
 class OrderItemsController < ApplicationController
   def destroy
     @order_item = OrderItem.find(params[:id])
-    if @order_item.order.draft?
-      @order_item.destroy
-      @order_item.order.calculate_total_price
-      redirect_to @order_item.order, notice: 'Item was removed from the order.'
-    else
-      redirect_to @order_item.order, notice: 'Items can be removed only from draft orders.'
-    end
+    return unless @order_item.order.draft?
+
+    @order_item.destroy
+    @order_item.order.calculate_total_price
+    redirect_to @order_item.order, notice: t('.destroy.notice')
   end
 end
