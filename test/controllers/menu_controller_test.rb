e3154c611f453625f0260329172f01cb1c3805df
Yaro Shm <yshmarov@gmail.com>
2025-01-04 19:36:27 +0100

    test menu#index

---
+++ test/controllers/menu_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class MenuControllerTest < ActionDispatch::IntegrationTest
+  test 'index' do
+    get menu_url
+    assert_response :success
+
+    menu_item_drink = menu_items(:monster_classic)
+    menu_item_food = menu_items(:rafaello)
+
+    assert_match menu_item_drink.name, response.body
+    assert_match menu_item_food.name, response.body
+
+    # filter by category
+    get menu_url(menu_category: 'food')
+    assert_response :success
+    assert_match menu_item_food.name, response.body
+    assert_no_match menu_item_drink.name, response.body
+  end
+end
