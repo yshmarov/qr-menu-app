f969533d4e9c990a654ec428f3d2066ab61e32be
Yaro Shm <yshmarov@gmail.com>
2025-01-05 15:53:35 +0100

    Update shop_controller_test.rb

--- test/controllers/shop_controller_test.rb
+++ test/controllers/shop_controller_test.rb
@@ -1,4 +1,22 @@
 require "test_helper"
 
 class ShopControllerTest < ActionDispatch::IntegrationTest
+  test "add to cart" do
+    get products_path
+    assert_response :success
+
+    product = products(:monster_classic)
+    assert_difference "Order.count", 1 do
+      assert_difference "OrderItem.count", 1 do
+        post add_to_cart_path(product.id)
+      end
+    end
+    assert_redirected_to products_path
+    assert_equal "#{product.name} added to cart", flash[:notice]
+
+    order = Order.last
+    assert_equal product, order.products.first
+    assert_equal 1, order.order_items.first.quantity
+    assert_equal "To go", order.delivery_details
+  end
 end
