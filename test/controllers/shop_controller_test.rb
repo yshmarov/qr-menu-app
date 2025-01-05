ff115f990959c747c7e71a65cdde89a1375c7d9f
Yaro Shm <yshmarov@gmail.com>
2025-01-05 20:16:20 +0100

    Update shop_controller_test.rb

--- test/controllers/shop_controller_test.rb
+++ test/controllers/shop_controller_test.rb
@@ -18,5 +18,17 @@ class ShopControllerTest < ActionDispatch::IntegrationTest
     assert_equal product, order.products.first
     assert_equal 1, order.order_items.first.quantity
     assert_equal "To go", order.delivery_details
+
+    product2 = products(:rafaello)
+    post add_to_cart_path(product2.id)
+    assert_redirected_to products_path
+    assert_equal "#{product2.name} added to cart", flash[:notice]
+    assert_equal 2, order.order_items.map(&:quantity).sum
+    assert_equal 2, order.order_items.count
+
+    post add_to_cart_path(product2.id)
+    order.reload
+    assert_equal 3, order.order_items.map(&:quantity).sum
+    assert_equal 2, order.order_items.count
   end
 end
