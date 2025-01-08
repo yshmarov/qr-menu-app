40fae685ef4d4caa2608098bcaaf8c6fb858da67
Yaro Shm <yshmarov@gmail.com>
2025-01-08 09:01:43 +0100

    order status should not be editable by customer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,26 +12,35 @@ class OrdersController < ApplicationController
 
   def update
     return redirect_to @order, notice: "Order must have at least one item" if @order.order_items.empty?
-    # TODO: the status should not be editable from here. it can only be moved from Draft to Submitted when payment is successful
-    # TODO: implement payment
-    # change status (pay)
-    return unless @order.update(order_params)
 
-    notice_text = if order_params[:status].present?
-                    "Ordered!"
-    else
-                    "Your rating: #{order_params[:rating]}"
+    case @order.status
+    when "draft"
+      notice = handle_payment
+    when "done"
+      notice = handle_rating
     end
-    redirect_to order_url(@order), notice: notice_text
+
+    redirect_to order_url(@order), notice:
   end
 
   private
+
+  def handle_payment
+    # TODO: implement payment
+    @order.submitted!
+    "Ordered!"
+  end
+
+  def handle_rating
+    @order.update(order_params)
+    "Your rating: #{order_params[:rating]}"
+  end
 
   def set_order
     @order = @my_orders.find(params[:id])
   end
 
   def order_params
-    params.require(:order).permit(:status, :rating)
+    params.require(:order).permit(:rating)
   end
 end
