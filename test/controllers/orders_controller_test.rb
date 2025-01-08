ec85fe123206292332e96989484483fb50c45f59
Yaro Shm <yshmarov@gmail.com>
2025-01-08 14:25:01 +0100

    Update orders_controller_test.rb

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -14,6 +14,20 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     patch order_url(order), params: { order: { status: "submitted" } }
     assert_response :redirect
     assert_redirected_to order_url(order)
-    assert_equal "Ordered!", flash[:notice]
+    assert_equal "Order submitted! We are preparing it for you.", flash[:notice]
+
+    patch order_url(order), params: { order: { status: "delivery", rating: 5 } }
+    assert_equal "submitted", order.reload.status
+    assert_nil order.reload.rating
+
+    order.done!
+    assert_equal "done", order.reload.status
+    patch order_url(order), params: { order: { status: "delivery", rating: 4 } }
+    assert_equal "done", order.reload.status
+    assert_not_nil order.reload.rating
+
+    # rating
+    patch order_url(order), params: { order: { rating: 5 } }
+    assert_equal 5, order.reload.rating
   end
 end
