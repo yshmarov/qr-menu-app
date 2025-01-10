35b57052fb985895555b3b8bf970d42fceb3b48c
Yaro Shm <yshmarov@gmail.com>
2025-01-10 22:57:10 +0100

    load fontawesome CSS, not JS (prevent jump on page load)

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.1/js/all.js"
-
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
