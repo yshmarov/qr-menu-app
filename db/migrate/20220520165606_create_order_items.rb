b36d13432c12c6952051ad225a771d9b859ec5d9
Yaro Shm <yshmarov@gmail.com>
2025-01-11 00:14:18 +0100

    clean up migrations

--- db/migrate/20220520165606_create_order_items.rb
+++ db/migrate/20220520165606_create_order_items.rb
@@ -5,6 +5,7 @@ class CreateOrderItems < ActiveRecord::Migration[7.0]
       t.references :product, null: false, foreign_key: true
       t.integer :quantity, default: 0, null: false
       t.bigint :total_price, default: 0, null: false
+      t.bigint :price, default: 0, null: false
 
       t.timestamps
     end
--- db/migrate/20250110231101_add_price_to_order_items.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToOrderItems < ActiveRecord::Migration[8.0]
-  def change
-    add_column :order_items, :price, :bigint, null: false, default: 0
-  end
-end
