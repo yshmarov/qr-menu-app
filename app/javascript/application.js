74236a286dfc54016875984dd55e817644a1305e
Yaro Shm <yshmarov@gmail.com>
2025-01-11 00:27:12 +0100

    clean up fontawesome js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "@fortawesome/fontawesome-free"
+import '@hotwired/turbo-rails'
+import 'controllers'
