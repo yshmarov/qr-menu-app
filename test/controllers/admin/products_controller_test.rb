e7e49f1c7bf38a12db9f0ab4831cdd433177c44f
Yaro Shm <yshmarov@gmail.com>
2025-01-11 22:12:45 +0100

    Update products_controller_test.rb

--- test/controllers/admin/products_controller_test.rb
+++ test/controllers/admin/products_controller_test.rb
@@ -14,4 +14,37 @@ class Admin::ProductsControllerTest < ActionDispatch::IntegrationTest
     get admin_products_url, headers: @headers
     assert_response :success
   end
+
+  test "new" do
+    get new_admin_product_url, headers: @headers
+    assert_response :success
+  end
+
+  test "create" do
+    assert_difference("Product.count") do
+      post admin_products_url, headers: @headers, params: {
+        product: { name: "Product", price: 1, category: "food", image_url: "image_url" }
+      }
+    end
+
+    assert_redirected_to admin_products_url
+  end
+
+  test "update" do
+    product = products(:monster_classic)
+    patch admin_product_url(product), headers: @headers, params: {
+      product: { name: "Product" }
+    }
+
+    assert_redirected_to admin_products_url
+  end
+
+  test "destroy" do
+    product = products(:monster_classic)
+    assert_difference("Product.count", -1) do
+      delete admin_product_url(product), headers: @headers
+    end
+
+    assert_redirected_to admin_products_url
+  end
 end
