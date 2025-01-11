08de5681ea4be3d01480e54251c49f70132c4671
Yaro Shm <yshmarov@gmail.com>
2025-01-11 19:12:44 +0100

    test amounts in deleting order item

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -23,10 +23,12 @@ class OrderItemsControllerTest < ActionDispatch::IntegrationTest
     order = Order.last
     assert_equal 1, order.order_items.count
     assert_equal 2, order.order_items_quantity
+    assert_equal 1198, order.total_price
 
     delete order_order_item_url(order, order.order_items.first)
     assert_redirected_to order_path(order)
     assert_equal 0, order.order_items.count
     assert_equal 0, order.reload.order_items_quantity
+    assert_equal 0, order.total_price
   end
 end
