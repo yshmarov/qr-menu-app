ef0ac1c7df509231b139af45538dfe295d2f2a60
Yaro Shm <yshmarov@gmail.com>
2025-01-11 19:21:32 +0100

    clean up unused global vars

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -10,7 +10,7 @@ class ShopController < ApplicationController
     # set delivery delivery
     delivery_details = session[:table_delivery].presence || "To go"
     # find or create order
-    @order = @current_order.presence || Order.create(
+    order = @current_order.presence || Order.create(
       status: Order.statuses[:draft],
       delivery_details:,
       user_id: current_guest_id
@@ -19,16 +19,16 @@ class ShopController < ApplicationController
     session[:table_delivery] = nil
 
     # add to cart
-    @product = Product.find(params[:product_id])
-    @order_item = @order.order_items.find_or_create_by(product: @product)
+    product = Product.find(params[:product_id])
+    order_item = order.order_items.find_or_create_by(product:)
     # add +1 item to cart
-    @order_item.increment!(:quantity)
+    order_item.increment!(:quantity)
     # balance calculation
-    @order_item.calculate_total_price
+    order_item.calculate_total_price
 
-    @current_order = @order
+    @current_order = order
 
-    notice = "#{@product.name} added to cart"
+    notice = "#{product.name} added to cart"
 
     redirect_to products_path, notice:
   end
