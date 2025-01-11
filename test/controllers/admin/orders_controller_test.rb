bbd85c09069e4fa00e0fb331684e5bab589678ff
Yaro Shm <yshmarov@gmail.com>
2025-01-11 22:17:41 +0100

    Update orders_controller_test.rb

--- test/controllers/admin/orders_controller_test.rb
+++ test/controllers/admin/orders_controller_test.rb
@@ -14,4 +14,19 @@ class Admin::OrdersControllerTest < ActionDispatch::IntegrationTest
     get admin_orders_url, headers: @headers
     assert_response :success
   end
+
+  test "show" do
+    order = Order.create!(status: Order.statuses[:submitted], user_id: 1)
+    get admin_order_url(order), headers: @headers
+    assert_response :success
+  end
+
+  test "update" do
+    order = Order.create!(status: Order.statuses[:submitted], user_id: 1)
+    patch admin_order_url(order), headers: @headers, params: {
+      order: { status: Order.statuses[:processing] }
+    }
+
+    assert_redirected_to admin_order_url(order)
+  end
 end
