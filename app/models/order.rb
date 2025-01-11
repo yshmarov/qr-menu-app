8ccccdec723d6e733d74dd82764db8ab8762043c
Yaro Shm <yshmarov@gmail.com>
2025-01-11 22:17:48 +0100

    order - validate user_id

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,6 +8,7 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
 
+  validates :user_id, presence: true
   validates :status, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }, allow_blank: true
 
